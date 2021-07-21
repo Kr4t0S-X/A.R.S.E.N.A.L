79b679baa609de8533a4f62c095307d5a033f8ef
KraaatOS <ghanime.charles@gmail.com>
2021-07-21 23:21:48 +0200

    implementing tabs as data structures

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+'use strict';
+
 const Discord = require("discord.js");
 const config = require("./config.json")
 
 const client = new Discord.Client();
 const prefix = "!";
+const { Client, MessageEmbed } = require('discord.js');
 
 client.on("message", function(message) {
 	if (message.author.bot) return;
@@ -17,9 +20,36 @@ client.on("message", function(message) {
 		 message.reply('Pong! This message had a latency of ${timeTaken}ms.');
 	 }
 
-	if (command === "khoraprime") {
-		message.reply('https://imgur.com/a/WQRUqqX');
+/* WARFRAME BUILDS */
+
+	/**
+const wf_tab = new Array(255);
+const wf_build = new Array(255);
+
+	wf_tab = [ [Ash], [Atlas], [Banshee], [Baruuk], [Chroma], [Ember], [Equinox], [Excalibur], [Frost], [Gara], [Garuda], [Gauss], [Grendel], [Harrow], [Hildryn], [Hydroid], [Inaros], [Ivara], [Khora], [Limbo], [Loki], [Mag], [Mesa], [Mirage], [Nekros], [Nezha], [Nidus], [Nova], [Nyx], [Oberon], [Octavia], [Protea], [Revenant], [Rhino], [Saryn], [Titania], [Trinity], [Valkyr], [Vauban], [Volt], [Wisp], [Wukong], [Zephyr] ];
+
+	wf_builds = [ ['[Build regulators]\n(https://imgur.com/Acyx6wX)\n[Build general]\n(https://imgur.com/GA3FLZe)';
+var i = new int;
+	let i = 0;
+
+	while (i <= 100) {
+
 	}
+
+		if (command === wf_tab[i]) {
+			message.reply('https://imgur.com/a/WQRUqqX');
+	}
+**/
+	if (command === "mesa") {
+		const embed = new MessageEmbed()
+		.setTitle('Mesa')
+		.setColor(0xff0000)
+		.setDescription('Regulators\n[Build](https://imgur.com/Acyx6wX)\nGeneral\n[Build](https://imgur.com/GA3FLZe)');
+		message.channel.send(embed);
+	}
+
+/* WEAPON BUILDS  */
+
 });
 
 client.login(config.BOT_TOKEN);
