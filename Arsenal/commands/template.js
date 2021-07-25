39fc778e137c49c31bf611bf50f432df51cdfa2d
KraaatOS <ghanime.charles@gmail.com>
2021-07-25 14:51:32 +0200

    a bit of cleaning never hurts

--- Arsenal/commands/template.js
+++ Arsenal/commands/template.js
@@ -10,9 +10,9 @@ module.exports = {
 		/* Color of the embed message, in hexa mode */
 			.setColor(0xff00ff)
 		/* '**text**' has to be used for bold, 
-		 * '\n' to go to the next line, you need 2 
-		 * if you want an empty line. For hyperlinks, 
-		 * the format is always [text of the link](https link)
+		 * '\n' to go to the next line,
+		 * you need 2 if you want an empty line.
+		 * For hyperlinks, the format is always [text to show for link](link)
 		 */
 			.setDescription('**Warframe :**\nGeneral\n[Build](https://www.youtube.com/watch?v=dQw4w9WgXcQ)\nArbitration\n[Build](https://www.youtube.com/watch?v=dQw4w9WgXcQ)\n\n**Pet :**\nGeneral\n[Build](https://www.youtube.com/watch?v=dQw4w9WgXcQ)\n\n**Stat Stick :**\nGeneral\n[Build](https://www.youtube.com/watch?v=dQw4w9WgXcQ)\n\n**Exhalted Weapon :**\nGeneral\n[Build](https://www.youtube.com/watch?v=dQw4w9WgXcQ)');
 		message.channel.send(embed);
