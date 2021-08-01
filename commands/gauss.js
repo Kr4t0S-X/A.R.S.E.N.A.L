a1e4ec84c7fd80dfef326fcff0f854b81474eb15
KraaatOS <ghanime.charles@gmail.com>
2021-08-01 22:36:30 +0200

    still trying to fix it

--- commands/gauss.js
+++ commands/gauss.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 
 module.exports = {
 	/* The name will be the commands to enter after !build to trigger the bot  */
-	name: 'template',
-	description: 'description',
+	name: 'Gauss',
+	description: 'Gauss Builds',
 	execute(message, args) {
 		const embed = new Discord.MessageEmbed()
 		/* Title of the embed message visibale by User */
-			.setTitle('Name of frame')
+			.setTitle('Gauss')
 		/* Color of the embed message, in hexa mode */
 			.setColor(0xff00ff)
 		/* '**text**' has to be used for bold, 
