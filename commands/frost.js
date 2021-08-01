fdb4256aede591b1293bb7d423c207232406e699
KraaatOS <ghanime.charles@gmail.com>
2021-08-01 20:28:21 +0200

    implementing new warframes

--- commands/frost.js
+++ commands/frost.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 
 module.exports = {
 	/* The name will be the commands to enter after !build to trigger the bot  */
-	name: 'zexal',
-	description: 'Queen of Dramas and Toxicity',
+	name: 'frost',
+	description: 'Frost Builds',
 	execute(message, args) {
 		const embed = new Discord.MessageEmbed()
 		/* Title of the embed message visibale by User */
-			.setTitle('Queen of Dramas and Toxicity')
+			.setTitle('Frost')
 		/* Color of the embed message, in hexa mode */
 			.setColor(0xff00ff)
 		/* '**text**' has to be used for bold, 
@@ -15,7 +15,7 @@ module.exports = {
 		 * you need 2 if you want an empty line.
 		 * For hyperlinks, the format is always [text to show for link](link)
 		 */
-			.setDescription('I like dramas and being [toxic](https://www.youtube.com/watch?v=LOZuxwVk7TU)');
+			.setDescription('**Warframe :**\nGeneral\n[Build](https://overframe.gg/build/48143)\nSpam Globe\n[Build](https://overframe.gg/build/48141)\nNuke\n[Build](https://overframe.gg/build/48145)');
 		message.channel.send(embed);
 	}
 }
